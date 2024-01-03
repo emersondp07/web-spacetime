cd053781f141cf3d2e862cb102f11195215b407e
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2024-01-03 00:22:20 -0300

    created route new memory, middleware authenticate and link in the button register memory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Bai_Jamjuree as BaiJamhuree, Roboto_Flex as Roboto } from "next/font/google";
+import { cookies } from "next/headers";
 import { ReactNode } from "react";
+import { Copyright } from "../components/Copyright";
+import { Hero } from "../components/Hero";
+import { Profile } from "../components/Profile";
+import { SignIn } from "../components/SignIn";
 import "./globals.css";
 
 const roboto = Roboto({ subsets: ["latin"], variable: '--font-roboto' }); 
@@ -12,9 +17,36 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${baiJamhuree.variable} font-sans bg-gray-900 text-gray-100`}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamhuree.variable} font-sans bg-gray-900 text-gray-100`}>
+        <main className="grid grid-cols-2 min-h-screen">
+          {/* Left */}
+          <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[280px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+
+            {/* Stripes */}
+            <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"></div>
+
+            {/* Sign In */}
+            {isAuthenticated ? <Profile /> :<SignIn />}
+
+            {/* Hero */}
+            <Hero />
+
+            {/* Copyright */}
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import nlwLogo from '../assets/nlw-spacetime-logo.svg';
 
 export function Hero () {
@@ -14,9 +15,9 @@ export function Hero () {
         </p>
       </div>
 
-      <a href="" className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600">
+      <Link href="/memories/new" className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600">
           CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
