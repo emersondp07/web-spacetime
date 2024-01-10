774f9b04458c2daf264292e06ff7bc8ef6f05557
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2024-01-09 22:46:30 -0300

    fixed error: add hook route and new config images in Next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '172.20.170.67']
+    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_URL_GITHUB_AVATAR}`,
+        port: '',
+        pathname: '/u/**'
+      },
+      {
+        protocol: 'http',
+        hostname: `${process.env.NEXT_PUBLIC_IP_IMAGES}`,
+        port: '3334',
+        pathname: '/uploads/**'
+      },
+    ]
   }
 }
 
