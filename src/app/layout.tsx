9d8d72c58ec9648b2901c90846f3fcac6f7154a7
Emerson Dantas <emerson.dantaspereira@hotmail.com>
2024-01-09 22:17:56 -0300

    finished application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover overflow-y-scroll max-h-screen">
             {children}
           </div>
         </main>
